edc923e26de30f952dd4201169844777ebf07159
bpsik <01178579@pw.edu.pl>
2025-04-15 13:24:14 +0200

    Cookie cutter setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this project is to understand the key factors influencing customers of the Nocarz booking platform when they choose to book longer stays.',
+    author='Bartosz Psik / Maksymilian Baj',
+    license='MIT',
+)
